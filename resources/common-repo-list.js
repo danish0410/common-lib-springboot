0fd6c224165d5fff81b5b15035ea59edd3b84445
Thanigaivelan S <thanigaivelan.sekar@gmail.com>
2025-07-12 07:01:49 +0530

    adding common-repo-list.js

---
+++ resources/common-repo-list.js
@@ -0,0 +1,7 @@
+{
+  "repo-name": "springboot-role",
+  "project_dir": ".",
+  "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
+  "dockerhub_username": "thanigai2808",
+  "git_credentials_id": "private-key-jenkins"
+}
