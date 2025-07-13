6028f6934d1694e905d032a58bf0ff131ee93c3b
danish0410 <thanigai_velan@yahoo.com>
2025-07-13 06:57:27 +0530

    Update common-repo-list.js

--- resources/common-repo-list.js
+++ resources/common-repo-list.js
@@ -1,7 +1,11 @@
 {
-  "repo-name": "springboot-role",
-  "project_dir": ".",
-  "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
-  "dockerhub_username": "thanigai2808",
-  "git_credentials_id": "private-key-jenkins"
+  "springboot": [
+    {
+      "repo-name": "springboot-role",
+      "project_dir": ".",
+      "git-url": "git@github.com:thani2808/Dev-role-Springboot-proj.git",
+      "dockerhub_username": "thanigai2808",
+      "git_credentials_id": "private-key-jenkins"
+    }
+  ]
 }
